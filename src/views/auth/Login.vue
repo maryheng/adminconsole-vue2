9731444680b97278f4b035f45248cc8ae6f599ac
Mary Heng <heng_mary@live.com>
2017-08-05 16:27:37 +0800

    Validation for login.

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -11,11 +11,15 @@
           <form v-on:submit.prevent="loginFormSubmit()">
             <label class="label">Username</label>
             <p class="control">
-              <input class="input" v-model="req.body.username" type="text" placeholder="username">
+              <input v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('username') }"
+              name="username" class="input" v-model="req.body.username" type="text" placeholder="username">
+              <span v-show="errors.has('username')" class="help is-danger">{{ errors.first('username') }}</span>                   
             </p>
             <label class="label">Password</label>
             <p class="control">
-              <input class="input" v-model="req.body.password" type="password" placeholder="password">
+              <input v-validate="'required|min:8'" :class="{'input': true, 'is-danger': errors.has('password') }"
+              name="password" class="input"v-model="req.body.password" type="password" placeholder="password">
+              <span v-show="errors.has('password')" class="help is-danger">{{ errors.first('password') }}</span>                     
             </p>
   
             <br><br>
@@ -28,6 +32,9 @@
       </div>
     </div>
   </div>
+          <!-- Simplert Notification -->
+        <simplert :useRadius="true" :useIcon="true" ref="simplert">
+        </simplert>
 </div>
 </template>
 
@@ -35,8 +42,12 @@
 import { loginUrl } from '../../config'
 import axios from 'axios'
 import router from '../../router'
+import Simplert from 'vue2-simplert/src/components/simplert'
 
 export default {
+  components: {
+    Simplert
+  },
   data () {
     return {
       req: {
@@ -66,28 +77,42 @@ export default {
   // },
   methods: {
     loginFormSubmit () {
-      axios.post(loginUrl, {
-        username: this.req.body.username,
-        password: this.req.body.password
-      })
-      .then((response) => {
-        console.log(response)
-        console.log(response.data.token)
+      let self = this
+      self.$validator.validateAll().then(result => {
+        if (result) {
+          axios.post(loginUrl, {
+            username: self.req.body.username,
+            password: self.req.body.password
+          })
+          .then((response) => {
+            // set token and cookie expiry to local storage
+            window.localStorage.setItem('access_token', response.data.token)
+            window.localStorage.setItem('cookie_expiry', new Date(Date.now() + 28800000).toString())
 
-        // set token and cookie expiry to local storage
-        window.localStorage.setItem('access_token', response.data.token)
-        window.localStorage.setItem('cookie_expiry', new Date(Date.now() + 28800000).toString())
-
-        // Include token in all request headers
-        if (window.localStorage.getItem('access_token') !== null) {
-          // axios.defaults.headers.common['authorization'] = 'Bearer ' + window.localStorage.getItem('access_token')
-          axios.defaults.headers.common['authorization'] = window.getToken()
+            // Include token in all request headers
+            if (window.localStorage.getItem('access_token') !== null) {
+              // axios.defaults.headers.common['authorization'] = 'Bearer ' + window.localStorage.getItem('access_token')
+              axios.defaults.headers.common['authorization'] = window.getToken()
+            }
+            router.push({ path: '/' })
+          })
+            .catch((error) => {
+              let errorAlert = {
+                title: 'Error',
+                message: error.response.data.message.message,
+                type: 'error'
+              }
+              self.$refs.simplert.openSimplert(errorAlert)
+            })
+          return
         }
-        router.push({ path: '/' })
+        let errorAlert = {
+          title: 'Error',
+          message: 'Some fields are incorrect!',
+          type: 'error'
+        }
+        self.$refs.simplert.openSimplert(errorAlert)
       })
-        .catch((error) => {
-          console.log(error)
-        })
     }
   },
   created () {
@@ -98,6 +123,14 @@ export default {
 </script>
 
 <style>
+#boxForLogin .input {
+  width: 500px;
+}
+
+#boxForLogin button {
+  margin-bottom: 5%;
+}
+
 #boxForLogin .box {
   padding: 50px;
   margin-top: 20%;
